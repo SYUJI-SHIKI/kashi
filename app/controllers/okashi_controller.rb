4b180ca607d5a6d3bb749949a0c718ac40969041
SYUJI-SHIKI <syuzi00777@gmail.com>
2024-04-29 16:04:56 +0900

    デプロイ

--- app/controllers/okashi_controller.rb
+++ app/controllers/okashi_controller.rb
@@ -1,9 +1,5 @@
+
 class OkashiController < ApplicationController
   def index
-    import requests
-    # URL＋クエリパラメータ
-    url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=400040'
-    # APIリクエストを送信
-    tenki_data = requests.get(url).json()
   end
 end
